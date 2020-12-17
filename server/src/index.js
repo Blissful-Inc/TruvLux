fd60a98f22dff7383a687767e48495c0836fe7b1
xh88 <rarityrings333@gmail.com>
2020-12-17 18:38:42 -0500

    added client

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,47 @@
 const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const middlewares = require("./middlewares");
+const logs = require("./api/Logs");
+
+mongoose.connect("mongodb://localhost/truvlux", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection
+  .once("open", function () {
+    console.log("Database connected...");
+  })
+  .then("error", function (error) {
+    console.log("Connection error: ", error);
+});
+
 const app = express();
+app.use(morgan("common"));
+app.use(helmet());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "hello world",
+  });
+});
+
+app.use("/api/logs", logs);
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
-  console.log("Listening at http://localhost:${port}");
+  console.log(`Listening at http://localhost:${port}  || ---- >> `);
 });
